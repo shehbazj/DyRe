044d115c28a65d1985af76bd548680447f89e521
Shehbaz Jaffer <shehbazjaffer007@gmail.com>
2015-12-01 14:43:02 -0500

    added sample tables for raid4, raid5_la. Use ./initDMSetup; dmsetup create mydev table_file

--- tools/initDmsetup.sh
+++ tools/initDmsetup.sh
@@ -5,19 +5,20 @@ fi
 
 numDevs=${1-"1"}
 
+echo "number of devices = $numDevs"
 
 dmsetup remove mydev
 
-for i in [[ `seq 1 $numDevs` ]]; do 
+for i in `seq 1 $numDevs`; do 
 	losetup -d /dev/loop$i
 	rm -rf /tmp/$i
-	dd if=/dev/zero of=/tmp/$i bs=4096 count=1000
+	dd if=/dev/zero of=/tmp/$i bs=9000 count=10000
 	losetup /dev/loop$i /tmp/$i
 done
 
 # start gdb
 
-gdb ./dmsetup
+#gdb ./dmsetup
 
 
 #echo $numDevs
